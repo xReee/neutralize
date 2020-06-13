b4b0ada6a6531bce6c28b98d2f0f4b2d0fc80204
Izabella Melo <cmeloizabella@gmail.com>
2020-06-13 15:57:52 -0300

    deletando options e criando script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
@@ -9,7 +6,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: {schemes: [ 'http', 'https']},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
